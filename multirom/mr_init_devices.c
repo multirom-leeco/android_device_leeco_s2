d5d4436c82537d65fbee5220e4baba28c06b9be2
Rishabh Rao <admin@rishabhrao.net>
2017-01-28 11:10:09 +0530

    Added USB-OTG Support

--- multirom/mr_init_devices.c
+++ multirom/mr_init_devices.c
@@ -9,8 +9,10 @@ const char *mr_init_devices[] =
 {
     "/sys/class/graphics/fb0",
     "/sys/class/graphics/fb1",
+    "/sys/devices/virtual/graphics/fb0",
+    "/sys/devices/virtual/graphics/fb1",
 
-    "/sys/block/mmcblk0",
+    "/sys/block/mmcblk0/*",
     "/sys/block/mmcblk0rpmb",
     "/sys/devices/soc.0/7824900.sdhci",
     "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0",
@@ -22,12 +24,11 @@ const char *mr_init_devices[] =
     "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p27", // system
     "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p56", // userdata
     "/sys/devices/soc.0/7824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0rpmb", // RPMB
-    "/sys/bus/mmc",
+    "/sys/bus/mmc*",
     "/sys/bus/mmc/drivers/mmcblk",
     "/sys/module/mmc_core",
     "/sys/module/mmcblk",
 
-    // for keys and touchscreen
     "/sys/devices/soc.0/gpio_keys.109",
     "/sys/devices/soc.0/gpio_keys.109/input*",
     "/sys/devices/virtual/input*",
@@ -35,12 +36,13 @@ const char *mr_init_devices[] =
     "/sys/devices/soc.0*",
     "/sys/devices/soc.0/*",
 
-    // for adb
     "/sys/devices/virtual/tty/ptmx",
+    "/sys/class/misc/android_adb",
+    "/sys/class/android_usb/android0*",
+    "/sys/class/android_usb/android0/f_adb",
+    "/sys/bus/platform/drivers/android_usb",
+    "/sys/bus/usb",
 
-    // USB drive is in here
-
-    // Encryption
     "/sys/devices/virtual/misc/device-mapper",
     "/sys/devices/virtual/misc/ion",
     "/sys/devices/virtual/qseecom/qseecom",
